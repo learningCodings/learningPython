adbaa029cc20735523080608f76df4c296d38b18
HN <vaibhavdnalawade396@gmail.com>
2021-08-08 14:52:20 +0530

    lambda

---
+++ lambda.py
@@ -0,0 +1,22 @@
+# lambda Use
+
+# in python we can define dict under list or dict under dict
+
+people = [
+    
+    {"name" : "RDJ", "charcter" : "Tony"},
+    {"name" : "Chris H", "charcter" : "Thor"},
+    {"name" : "Tom H", "charc": "Loki"}
+]
+
+print(f"\nThe List Is: {people}")
+
+# python can not sort dict to dict I need to define function to sort it
+"""def f(person):
+    return person["name"]"""
+
+# to avoid use of this own defined code instead of it I can use lambda
+
+people.sort(key = lambda person: person["name"] )
+
+print(f"\n{people}")
